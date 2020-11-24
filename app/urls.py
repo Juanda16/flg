d6da656248a9556e2019f3089e63b3442cf804c3
juan <juand16@gmail.com>
2020-11-23 22:52:33 -0500

    endpoints updated

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index_view'),
-    path('donors', views.DonorView.as_view(), name='donor_view'),
-    path('donors/<int:pk>', views.DonorView.as_view(), name='donor_view_detail'),
+    path('aindex', views.index, name='index_view'),
+    path('api/v1/donors', views.DonorView.as_view(), name='donor_view'),
+    path('api/v1/donors/<int:pk>', views.DonorView.as_view(), name='donor_view_detail'),
         
 ]
