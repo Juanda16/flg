05db20ffabfe460c0972a7217f469d669be1daf0
juan <juand16@gmail.com>
2020-11-25 21:30:06 -0500

    Baic CRUD working

--- app/views.py
+++ app/views.py
@@ -7,31 +7,15 @@ from django.contrib.auth.models import User
 from datetime import date
 from django.views import View
 from django.http import QueryDict
-<<<<<<< HEAD
 from app.service import gettingUser,postingUser,puttingUser,deletingUser
-#import json
-=======
-from app.service import *
 import json
 from django.http import JsonResponse
 
 
 
 # Views here.
->>>>>>> db0b87fec8308048d0247d3eedb31541a1628ca7
 
 class DonorView(View):
-<<<<<<< HEAD
-        def get(self, request,*args,**kwargs):
-                donor = gettingUser(id=kwargs['pk'])
-                return HttpResponse(donor)
-    
-
-        def post(self, request, *args, **kwargs):
-                donor = postingUser(request)
-                return HttpResponse(donor)
-                #return HttpResponsedonor)
-=======
    def get(self, request, *args, **kwargs):
       donor = gettingUser (id = kwargs['pk'])
       return HttpResponse(donor)
@@ -45,8 +29,6 @@ class DonorView(View):
       user = User.objects.create_user(userName, email, password)
       Donor.objects.create(user=user,documentType=request.POST["documentType"], documentId=request.POST["documentId"])
       
-      
-
       return HttpResponse(user)
 
    def put(self,request,*args,**kwargs):
@@ -72,20 +54,12 @@ class DonorView(View):
 
          return HttpResponse("user doesn´t exist")
       
-      
+   def put(self,request,*args,**kwargs):
+      id=kwargs['pk']
+      donor=puttingUser(request,id)
+      return HttpResponse(donor)
 
-      
-    
-    
-
->>>>>>> db0b87fec8308048d0247d3eedb31541a1628ca7
-
-        def put(self,request,*args,**kwargs):
-                id=kwargs['pk']
-                donor=puttingUser(request,id)
-                return HttpResponse(donor)
-
-        def delete(self,request,**kwargs):
-               id=kwargs['pk']
-               deletingUser(request,id)
-               return HttpResponse("El usuario ha sido eliminado") 
+   def delete(self,request,**kwargs):
+      id=kwargs['pk']
+      deletingUser(request,id)
+      return HttpResponse("El usuario ha sido eliminado") 
