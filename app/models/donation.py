8696301be18e5bc21043bca851f83f28201e9f39
juan <juand16@gmail.com>
2020-12-05 15:31:32 -0500

    starting user login

---
+++ app/models/donation.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
---
+++ app/routes/donor/endpoints.py
@@ -0,0 +1,4 @@
+# todo lo relacionado con los endpoints del donante 
+
+#api/v1/donor/login
+#api/v1/donor/id
