e998d5fd0ee3192d4027a6a937b52344cf16427a
juan <juand16@gmail.com>
2020-12-05 23:32:10 -0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddDonorComponent } from './components/add-donor/add-donor.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ // { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+ // { path: 'tutorials', component: TutorialsListComponent },
+ // { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  { path: 'add', component: AddDonorComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
